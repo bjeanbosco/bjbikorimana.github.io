e9085582986493b2a78e88530111b31862c1cbf4
bjeanbosco <126228144+bjeanbosco@users.noreply.github.com>
2023-02-27 19:43:25 +0200

    Pages
    Webpage

---
+++ app.js
@@ -0,0 +1,69 @@
+let Content=document.getElementById('main');
+
+
+Content.addEventListener('mouseover',Change)
+function Change(){
+    Content.style.backgroundColor="yellow"
+    Content.innerHTML="Thank you for your visit!"
+    let img=document.createElement('img');
+    img.src="bosco.jpg";
+    img.width="200px";
+    img.height="200px";
+    Content.appendChild(img);
+};
+
+Content.addEventListener("mouseout",change);
+function change(){
+    Content.innerHTML="Web development & Appications"
+    Content.style
+
+Content.style.backgroundColor="white"
+}
+
+let button=document.getElementById('JoinBtn');
+button.addEventListener('click',changer);
+function changer(){
+   document.getElementById('sign_up').style.display="block";
+   button.style.display="none";
+}
+//the code below this line are all about form.html page
+
+function validateForm() {
+    let x = document.getElementById('name').value;
+    let z=document.getElementById('email').value;
+    let P=document.getElementById('password').value;
+    let p=document.getElementById('cpassword').value;
+
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    else if (z == "") {
+        alert("Email must be filled out");
+        return false;
+      }
+      
+      else if(P == "") {
+        alert("password must be filled  ***");
+        return false;
+      }
+      else if(p==""){
+   alert("You must confirm new password");
+   return false;
+      }
+      else if (p!=P) {
+        alert("***password mismatch");
+        return false;
+      }
+      else if(P.length<8) {
+        alert("*** password should have atleast 8 character");
+        return false;
+      }
+      else{
+        alert("Sign up successful!")
+        return true;
+      }
+  }
+
+  //the codes below this line are all for blogs page
+  
---
+++ index.js
@@ -0,0 +1,34 @@
+
+let Enroll=document.getElementById('enroll');
+Enroll.addEventListener('click',enroll)
+function enroll(){
+  let answer=prompt("Which one do you prefer? Type Coding or Piano");
+  if(answer==""){
+    alert('Enroll first')
+    return false;
+  }
+ else if(answer=="Coding"){
+
+let aTag = document.createElement('a');
+aTag.innerHTML="Learn More On Coding Courses";
+aTag.href="https://www.w3schools.com/";
+aTag.title="programing course";
+aTag.target="_blank";
+document.getElementById('piano').appendChild(aTag);
+Enroll.innerHTML="Enrolled"
+  }
+  else if(answer=="Piano"){
+    let Tag = document.createElement('a');
+Tag.innerHTML="Learn More on Piano";
+Tag.href="https://www.youtube.com/shorts/-EWj_x5txNg";
+Tag.title="piano courses";
+Tag.target="_blank";
+document.getElementById('piano').appendChild(Tag);
+Enroll.innerHTML="Enrolled"
+  }
+  else{
+    document.getElementById('piano').innerHTML="We don't have course for you!";
+  }
+
+}
+let Nav=document.querySelectorAll('nav');
